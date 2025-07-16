764bafa12c5f1d31d3f770b85236875724fbc69f
Erykah <erkahwanga@gmail.com>
2025-07-16 11:02:28 +0300

    Add Admin Dashboard, Report Details, and Map View

---
+++ src/pages/AdminDashboard.jsx
@@ -0,0 +1,12 @@
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { getReports } from '../features/admin/adminSlice';
+import { updateStatus } from '../features/admin/adminAPI';
+
+const AdminDashboard = () => {
+  const dispatch = useDispatch();
+  const { reports, loading, error } = useSelector(state => state.admin);
+
+  useEffect(() => {
+    dispatch(getReports());
+  }, [dispatch]);
